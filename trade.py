045dbda1813e66fca32347761df6ea1748ff84b1
sawyer@ubuntu <254091355@qq.com>
2021-04-19 13:50:16 +0800

    添加cli交易

---
+++ trade.py
@@ -0,0 +1,39 @@
+from trader import client
+import sys
+import getopt
+
+
+if __name__ == "__main__":
+    try:
+        option, args = getopt.getopt(sys.argv[1:], 'hc:p:n:')
+    except:
+        print("error params")
+
+    for opt, value in option:
+        if '-h' == opt:
+            print("""
+            trade cli:
+                -c code
+                -p price
+                -n num
+            """)
+        elif '-c' == opt:
+            code = value
+        elif '-p' == opt:
+            price = value
+        elif '-n' == opt:
+            num = value
+
+    df = client.position['position']
+    market = df.loc[df['stock_code'] == code]['market'].values[0]
+    name = df.loc[df['stock_code'] == code]['stock_name'].values[0]
+    if int(num) > 0:
+        if code[0] in [1]:
+            client.buy_bond(name, code, market, price, num)
+        else:
+            client.buy_stock(name, code, market, price, num)
+    else:
+        if code[0] in [1]:
+            client.sell_bond(name, code, market, price, 0 - int(num))
+        else:
+            client.sell_stock(name, code, market, price, 0 - int(num))
